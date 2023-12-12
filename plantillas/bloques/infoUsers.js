c4be7fe266d8f94e339af23601fa6e84ca62136b
Maikel Andres Cisternas Mercado <mai.cisternas@duocuc.cl>
2023-12-12 20:26:15 -0300

    Corrección orden user

--- plantillas/bloques/infoUsers.js
+++ plantillas/bloques/infoUsers.js
@@ -12,10 +12,10 @@ export function getUsuarios() {
     let listaUsuarios = [
         new Usuario('11.347.632-K', 'lau.puentes@gmail.com', 'Lautaro Puentes', 16, 932547238, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
         new Usuario('12.345.670-K', 'ric.parker@gmail.com', 'Rick Parker', 19, 912341234, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
+        new Usuario('13.123.456-2', 'mar.miller@gmail.com', 'Mario Miller', 29, 912345678, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
         new Usuario('13.345.678-K', 'suz.labor@gmail.com', 'Suzana Labor', 25, 912344321, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
         new Usuario('14.345.696-K', 'gal.guzman@gmail.com', 'Galit Guzman', 32, 912438765, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
         new Usuario('15.345.678-K', 'aug.veniam@gmail.com', 'Augusto Veniam', 29, 912345678, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
-        new Usuario('13.123.456-2', 'mar.miller@gmail.com', 'Mario Miller', 29, 912345678, 'Condominio Costa del Mar - Manuel Francke, Quintero'),
     ];
     return listaUsuarios;
 }
